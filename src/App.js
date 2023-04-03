5786f41ea0e833143ee8ea6985e174ace3abc05b
XiaoKun Chen <chenx137@msu.edu>
2023-04-03 16:42:16 -0400

    JS changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { supabase } from './supabaseClient';
 import logo from './logo.svg';
 import './App.css';
-import { set } from 'lodash';
+
 
 const liblogo = {
   title: 'library',
